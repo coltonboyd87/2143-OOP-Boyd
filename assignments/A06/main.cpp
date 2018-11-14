f62026a73f93bed23be402a0aa47b0ad45ff4591
Colton Boyd <35539405+coltonboyd87@users.noreply.github.com>
2018-11-14 10:36:08 -0600

    Update main.cpp

--- assignments/A06/main.cpp
+++ assignments/A06/main.cpp
@@ -25,6 +25,8 @@ int main()
 {
 	RenderWindow window(VideoMode(500, 400), "SFML Clock");
 	counter clock1(25, 0, 50, 50);
+	counter clock2(25, 0);
+	counter clock3(0, 25, 450, 350);
 	clock1.setColors(Color::Black, Color::White, Color::Red);
 
 	while (window.isOpen())
@@ -35,12 +37,16 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
-		if (window.isOpen() && clock1.End())
+		if (window.isOpen() && clock1.End() &&clock2.End() &&clock3.End())
 			window.close();
 
 		window.clear();
 		window.draw(clock1);
-		clock1.tUpdate();
+		window.draw(clock2);
+		window.draw(clock3);
+		//clock1.tUpdate();
+		clock2.tUpdate();
+		clock3.tUpdate();
 		window.display();
 	}
 	return 0;
